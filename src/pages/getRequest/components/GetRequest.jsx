58e8b475d42cb8ef1b8a5aa65395e0d720567871
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2024-09-27 00:45:40 +0500

    get request topic is covered

--- src/pages/getRequest/components/GetRequest.jsx
+++ src/pages/getRequest/components/GetRequest.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export function GetRequest() {
-  return <div>this is get request page</div>;
+export class GetRequest extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      errorMsg: [],
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        this.setState({ posts: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ errorMsg: "Error retrieving data" });
+      });
+  };
+
+  render() {
+    const { posts, errorMsg } = this.state;
+    return (
+      <>
+        <div>
+          list of posts
+          {posts.length > 0
+            ? posts.map((post) => <div key={post.id}>{post.title}</div>)
+            : "abc"}
+          {errorMsg ? <div>{errorMsg}</div> : null}
+        </div>
+      </>
+    );
+  }
 }
+
+export default GetRequest;
