4353bc68c85b1e5e79b6db0b6bc6e9bce8e9340a
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2024-09-29 00:16:29 +0500

    counter is practiced with useReducer

--- src/pages/useReducer/components/UseReducer.jsx
+++ src/pages/useReducer/components/UseReducer.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const initialValue = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialValue;
+    default:
+      return state;
+  }
+};
 
 export function UseReducer() {
-  return <div>this is use reducer page</div>;
+  const [count, dispatch] = useReducer(reducer, initialValue);
+  return (
+    <div>
+      <div>count - {count}</div>
+      <button onClick={() => dispatch("increment")}>increment</button>
+      <button onClick={() => dispatch("decrement")}>decrement</button>
+      <button onClick={() => dispatch("reset")}>reset</button>
+    </div>
+    // useReducer is imported from react
+    // useReducer takes two things as argument (a): reducer function and (b): initial value and return two things (a): newState and dispatch method which specify the action
+    // we need to define initial value and reducer function, reducer function takes two things as argument current state and action and return one thing which is newState;
+    // code snippet is practiced as above
+  );
 }
